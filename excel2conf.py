5d6a3624000327d15d5a34440b8ae0e6a8814f63
zhudonghua <zdhsoft@163.com>
2018-04-11 14:20:46 +0800

    去除了，cfg,sql,key等三种用不到的格式。

--- excel2conf.py
+++ excel2conf.py
@@ -117,112 +117,6 @@ def IsEmptyLine(paramTable, paramRow, paramFieldCount):
     else:
         return False
 
-
-def table2as3config(paramTable, paramDestFileName, paramUseFields, paramClassName):
-    nrows = paramTable.nrows
-    ncols = paramTable.ncols
-    col_index_list = range(ncols)
-
-    title_map_to_index = {}
-    field_flag = [["",False,0] for i in col_index_list]
-    
-    use_field_index = []        
-
-    for index in col_index_list:
-        field_name = paramTable.cell_value(0, index)
-        strTitle = field_name.replace(u"\n", u"").replace(u"\"", u"")   #取出引号和\n这样的字符
-        field_flag[index][0] = strTitle
-        if (field_name.rfind(u"\"") >= 0):
-            field_flag[index][1] = True
-            field_flag[index][2] = 1
-        title_map_to_index[strTitle] = index                            #得到字段名对应的下标
-
-    for use_field in paramUseFields:
-        if use_field in title_map_to_index:
-            use_field_index.append(title_map_to_index[use_field])
-    #生成类名
-    strClassName = u"[" + paramClassName + u"]\n";
-    #生成字段列表
-    strFieldList = u"<";
-    field_count = len(use_field_index)
-    if field_count > 0:
-        for i in range(field_count-1):
-            strFieldList += field_flag[use_field_index[i]][0]
-            strFieldList += ","
-        strFieldList += field_flag[use_field_index[field_count-1]][0]
-    strFieldList += ">\n"
-
-    #输出配置
-    dir = os.path.dirname(paramDestFileName)
-    if dir and not os.path.exists(dir):
-        os.makedirs(dir)    
-    f = codecs.open(paramDestFileName,"w","utf-8")
-    f.write(strClassName)
-    f.write(strFieldList)
-
-    for r in range(1, nrows):
-        if IsEmptyLine(table, r, ncols):  #跳过空行
-            continue
-        strTmp = u"#"
-        if field_count < 1:
-            continue
-        for i in range(field_count-1):
-            col_index = use_field_index[i]
-            strCell = CellToString(paramTable.cell_value(r, col_index))
-            if field_flag[col_index][1]:
-                strTmp += u"\"" + strCell + u"\""
-            else:
-                strTmp += strCell
-            strTmp += u","
-            if r == 1:
-                chFirst = strCell[0]
-                if chFirst == u'[':
-                    field_flag[col_index][2]=2
-        col_index = use_field_index[field_count-1]
-        strCell = CellToString(paramTable.cell_value(r, col_index))  
-        if field_flag[col_index][1]:
-            strTmp += u"\"" + strCell + u"\""
-        else:
-            strTmp += strCell
-        strTmp += u"\n"
-        f.write(strTmp);
-    f.close()
-    #生成类
-    
-    strPackageName = u"com.hxgd.cfg";
-    strClassName = paramClassName
-    as3 = codecs.open("./"+strClassName+u".as","w","utf-8")
-    as3.write(u"package " + strPackageName + u"\n")
-    as3.write(u"{\n")
-    as3.write(u"    public class " + strClassName + u" extends ConfigBase\n")
-    as3.write(u"    {\n")
-    as3.write(u"        public function " + strClassName + u"()\n")
-    as3.write(u"        {\n")
-    as3.write(u"            super();\n")
-    as3.write(u"        }\n")
-    as3.write(u"\n")
-    as3.write(u"        override public function DoLoad(paramRecord:Array):void\n        {\n")
-    for i in range(field_count):
-        col_index = use_field_index[i]
-        strFieldName = field_flag[col_index][0]
-        as3.write(u"            " + strFieldName + u" = paramRecord[" + str(i) + u"];\n")
-    as3.write(u"        }\n\n")
-
-    for i in range(field_count):
-        col_index = use_field_index[i]
-        strFieldName = field_flag[col_index][0]
-        iFlag = field_flag[col_index][2]
-        if iFlag == 0:
-            as3.write(u"        public var " + strFieldName + u":Number;\n")
-        elif iFlag == 1:
-            as3.write(u"        public var " + strFieldName + u":String;\n")
-        elif iFlag == 2:
-            as3.write(u"        public var " + strFieldName + u":Array=[];\n")
-    as3.write(u"    }\n}\n")
-    as3.close();
-    print "Create ",paramDestFileName," OK"
-    return
-
 def table2map(table, jsonfilename, mapTable, key):
     nrows = table.nrows
     ncols = table.ncols
@@ -391,123 +285,17 @@ def table2jsn(table, jsonfilename, mapTable):
     print "Create ",jsonfilename," OK"
     return
 
-def table2sql(table, jsonfilename, mapTable, mapParam):
-# {"name":"表名", "delete":False, "commit":True, "filter":"$Online>0 and $Name=='加速锦囊(1小时)'"}
-    nrows = table.nrows
-    ncols = table.ncols
-    hasMap = (len(mapTable) > 0)
-    dir = os.path.dirname(jsonfilename)
-    if dir and not os.path.exists(dir):
-        os.makedirs(dir)
+def table2ini(table, inifilename, mapTable):
 
-    #解析filter字符串
-    filterKey = []
-    filterString = ""
-    if "filter" in mapParam and len(mapParam["filter"]) > 0:
-        filterString = mapParam["filter"].decode("utf8")
-        filterKey = parseFilterKey(filterString)
-
-    tablename = destFileName[:destFileName.rfind(".")]    #用文件名做表名
-    tablename = tablename[tablename.rfind("\\")+1:]
-    if ("name" in mapParam) and len(mapParam["name"]) > 0:
-        tablename = mapParam["name"]
-
-    f = codecs.open(jsonfilename,"w","utf-8")
-    if not ("delete" in mapParam and not mapParam["delete"]):
-        f.write(u"truncate table " + tablename + u";\n")
-    f.write(u"set names 'utf8';\n")
-
-    if not(("commit" in mapParam) and not mapParam["commit"]):
-        f.write(u"set autocommit=0;\n")
-
-    for r in range(1, nrows):
-        if IsEmptyLine(table, r, ncols):  #跳过空行
-            continue
-        strTmp = u"insert into " + tablename + " set "
-        i = 0
-        strFilter = filterString
-        skip_row = False
-        get_this = not (len(filterKey) > 0)
-        for c in range(ncols):
-            title = table.cell_value(0,c)
-            isString = (title.rfind(u"\"") >= 0)
-            title = title.replace(u"\n", u"").replace(u"\"", u"")
-
-            if hasMap:
-                if not title in mapTable:
-                    continue
-                else:
-                    title = mapTable[title]
-
-            strCellValue = u""
-            CellObj = table.cell_value(r,c)
-            if type(CellObj) == unicode:
-                strCellValue = CellObj
-            elif type(CellObj) == float:
-                strCellValue = FloatToString(CellObj)
-            else:
-                strCellValue = str(CellObj)
-            
-            if isString:
-                strCellValue = strCellValue.replace(u"\n", u"").replace(u"'", u"\"")
-
-            if not get_this and title in filterKey:
-                if isString:
-                    strFilter = strFilter.replace(u"$" + title, u"\"" + strCellValue + u"\"")
-                else:
-                    strFilter = strFilter.replace(u"$" + title, strCellValue)
-
-                if strFilter.find("$") == -1:
-                    if not eval(strFilter):  #被过滤了
-                        skip_row = True
-                        break
-                    else:
-                        get_this = True     #确定了这行要
-
-            if i > 0:
-                delm = u", "
-            else:
-                delm = u""
-
-            if isString:
-                strTmp += delm + title + u" = '" + strCellValue + u"'"
-            else:
-                strTmp += delm + title + u" = " + strCellValue
-            i += 1
-
-        if skip_row:    #被过滤了
-            continue
-
-        strTmp += u";\n"
-        f.write(strTmp)
-
-    if not(("commit" in mapParam) and not mapParam["commit"]):
-        f.write(u"commit;\n")
-    f.write(u"\n")
-    f.close()
-    print "Create ",jsonfilename," OK"
-    return
-
-def table2ini(table, inifilename, mapTable, mapParam):
-# {"name":"section名", "filter":"$Online>0 and $Name=='加速锦囊(1小时)'"}
     nrows = table.nrows
     ncols = table.ncols
     hasMap = (len(mapTable) > 0)
     dir = os.path.dirname(inifilename)
     if dir and not os.path.exists(dir):
         os.makedirs(dir)
-        
-    #解析filter字符串
-    filterKey = []
-    filterString = ""
-    if "filter" in mapParam and len(mapParam["filter"]) > 0:
-        filterString = mapParam["filter"].decode("utf8")
-        filterKey = parseFilterKey(filterString)
 
     section = destFileName[:destFileName.rfind(".")]    #用文件名做节名
     section = section[section.rfind("\\")+1:]
-    if ("name" in mapParam) and len(mapParam["name"]) > 0:
-        section = mapParam["name"]
 
     f = codecs.open(inifilename,"w","utf-8")
     rs = 1
@@ -515,9 +303,6 @@ def table2ini(table, inifilename, mapTable, mapParam):
         if IsEmptyLine(table, r, ncols):  #跳过空行
             continue
         strTmp = u"[" + section + str(rs) + u"]\n"
-        strFilter = filterString
-        skip_row = False
-        get_this = not (len(filterKey) > 0)
         
         for c in range(ncols):
             title = table.cell_value(0,c)
@@ -540,23 +325,7 @@ def table2ini(table, inifilename, mapTable, mapParam):
                 strCellValue = str(CellObj)
 
             strCellValue = strCellValue.replace(u"\n", u"")  #去掉换行
-            if not get_this and title in filterKey:
-                if isString:
-                    strFilter = strFilter.replace(u"$" + title, u"\"" + strCellValue.replace(u"\"", u"") + u"\"")
-                else:
-                    strFilter = strFilter.replace(u"$" + title, strCellValue)
-
-                if strFilter.find("$") == -1:
-                    if not eval(strFilter):  #被过滤了
-                        skip_row = True
-                        break
-                    else:
-                        get_this = True     #确定了这行要
-
             strTmp += title + u" = "+ strCellValue + "\n"
-
-        if skip_row:    #被过滤了
-            continue
 
         rs += 1
         strTmp += u"\n"
@@ -570,36 +339,19 @@ def table2ini(table, inifilename, mapTable, mapParam):
     print "Create ",inifilename," OK"
     return
 
-def table2csv(table, csvfilename, mapTable, mapParam):
-# {"title":False, "filter":"$Online>0 and $Name=='加速锦囊(1小时)'"}
+def table2csv(table, csvfilename, mapTable):
     nrows = table.nrows
     ncols = table.ncols
     hasMap = (len(mapTable) > 0)
     dir = os.path.dirname(csvfilename)
     if dir and not os.path.exists(dir):
         os.makedirs(dir)
-    f = codecs.open(csvfilename,"w","utf-8")
-    
-    #解析filter字符串
-    filterKey = []
-    filterString = ""
-    if "filter" in mapParam and len(mapParam["filter"]) > 0:
-        filterString = mapParam["filter"].decode("utf8")
-        filterKey = parseFilterKey(filterString)
-
+    f = codecs.open(csvfilename,"w","utf-8-sig")  #为了excel能够正确打开，这里输出的csv文件是UTF-8 BOM格式的
     for r in range(nrows):
         if IsEmptyLine(table, r, ncols):  #跳过空行
             continue
         i = 0
-        strFilter = filterString
-        skip_row = False
-        get_this = not (len(filterKey) > 0)
         strTmp = u""
-
-        if r == 0 and ("title" in mapParam) and not (mapParam["title"]):
-            print("#########################################")
-            continue
-
         for c in range(ncols):
             title = table.cell_value(0,c)
             isString = (title.rfind(u"\"") >= 0)
@@ -628,122 +380,16 @@ def table2csv(table, csvfilename, mapTable, mapParam):
                 else:
                     strCellValue = str(CellObj).replace(u"\n", u"").replace(u",", u"")
 
-                if not get_this and title in filterKey:
-                    if isString:
-                        strFilter = strFilter.replace(u"$" + title, u"\"" + strCellValue.replace(u"\"", u"") + u"\"")
-                    else:
-                        strFilter = strFilter.replace(u"$" + title, strCellValue)
+                if isString:
+                    strTmp += delm + u"\""+ strCellValue + u"\""
+                else:
+                    strTmp += delm + strCellValue
 
-                    if strFilter.find("$") == -1:
-                        if not eval(strFilter):  #被过滤了
-                            skip_row = True
-                            break
-                        else:
-                            get_this = True     #确定了这行要
-
-                strTmp += delm + strCellValue
             i += 1
-
-        if skip_row:    #被过滤了
-            continue
-
         strTmp += u"\n"
         f.write(strTmp)
     f.close()
     print "Create ",csvfilename," OK"
-    return
-
-def table2key(table, jsonfilename, mapTable, mapParam):
-    nrows = table.nrows
-    ncols = table.ncols
-    hasMap = (len(mapTable) > 0)
-    dir = os.path.dirname(jsonfilename)
-    if dir and not os.path.exists(dir):
-        os.makedirs(dir)    
-    f = codecs.open(jsonfilename,"w","utf-8")
-    strTmp = u""
-
-    #解析filter字符串
-    filterKey = []
-    filterString = ""
-    if "filter" in mapParam and len(mapParam["filter"]) > 0:
-        filterString = mapParam["filter"].decode("utf8")
-        filterKey = parseFilterKey(filterString)
-
-    #var xxx = 
-    if ("var" in mapParam) and (len(mapParam["var"]) > 0):
-        strTmp += u"var " + mapParam["var"] + u" = "
-
-    strTmp += u"{\n"
-    f.write(strTmp)
-    rs = 0
-    for r in range(1, nrows): #跳过第1行标题
-        if IsEmptyLine(table, r, ncols):  #跳过空行
-            continue
-        strTmp = u"\t"
-        i = 0
-        strFilter = filterString
-        skip_row = False
-        get_this = not (len(filterKey) > 0)
-        for c in range(2):  #只处理最前面2列
-            title = table.cell_value(0,c)
-            isString = (title.rfind(u"\"") >= 0)
-            title = title.replace(u"\n", u"").replace(u"\"", u"")
-
-            if hasMap:
-                if not title in mapTable:
-                    continue
-                else:
-                    title = mapTable[title]
-
-            strCellValue = u""
-            CellObj = table.cell_value(r,c)
-            if type(CellObj) == unicode:
-                strCellValue = CellObj
-            elif type(CellObj) == float:
-                strCellValue = FloatToString(CellObj)
-            else:
-                strCellValue = str(CellObj)
-
-            if isString:
-                strCellValue = strCellValue.replace(u"\n", u"").replace(u"\"", u"")
-
-            if not get_this and title in filterKey:
-                if isString:
-                    strFilter = strFilter.replace(u"$" + title, u"\"" + strCellValue + u"\"")
-                else:
-                    strFilter = strFilter.replace(u"$" + title, strCellValue)
-
-                if strFilter.find("$") == -1:
-                    if not eval(strFilter):  #被过滤了
-                        skip_row = True
-                        break
-                    else:
-                        get_this = True     #确定了这行要
-
-            if i > 0:
-                delm = u":"
-            else:
-                delm = u""
-
-            if isString:
-                strTmp += delm + u"\""+ strCellValue + u"\""
-            else:
-                strTmp += delm + strCellValue
-            i += 1
-
-        if skip_row:    #被过滤了
-            continue
-        
-        if rs > 0:  #不是第1行
-            f.write(u",\n")
-        f.write(strTmp)
-        rs += 1
-
-    strTmp = u"\n}\n"
-    f.write(strTmp)
-    f.close()
-    print "Create ",jsonfilename," OK"
     return
 
 if __name__ == '__main__':
@@ -786,23 +432,17 @@ if __name__ == '__main__':
 
         suffix = destFileName[destFileName.rfind("."):].lower()
         if suffix == ".csv":
-            table2csv(destTable, destFileName, mapTable, mapParam)
+            table2csv(destTable, destFileName, mapTable)
         elif suffix == ".jsn" or suffix == ".js" or suffix == ".json":
             if retType["type"] == "map":
                 table2map(destTable, destFileName, mapTable, retType["key"])
             else:
                 table2jsn(destTable, destFileName, mapTable)
         elif suffix == ".conf":
-            table2ini(destTable, destFileName, mapTable, mapParam)
-        elif suffix == ".sql":
-            table2sql(destTable, destFileName, mapTable, mapParam)
-        elif suffix == ".key":
-            table2key(destTable, destFileName, mapTable, mapParam)
-        elif suffix == ".cfg":
-            table2as3config(destTable, destFileName, stFieldList,strClassName)
+            table2ini(destTable, destFileName, mapTable)
         else:
             print u"当前类型是:", suffix
-            print u"only support (jsn,js,json), csv, conf, sql, .key, .cfg format"
+            print u"only support (jsn,js,json), conf format"
             exit(1)
 
     print "All OK"
